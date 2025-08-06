e5697970812fd56c92904a21fe832a698b630957
josuerks <ksjosuer@gmail.com>
2025-08-06 18:44:58 +0200

    serveur corriger

--- app.py
+++ app.py
@@ -289,5 +289,6 @@ def update_adresse():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"Démarrage serveur sur le port {port} ...")
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+    
 
